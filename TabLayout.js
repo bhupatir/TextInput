d08f5e58093d79ad1a69f2fe51f6daaefc368497
Bhupathi Bade <bhupati@srivatech.com>
2016-09-01 23:41:51 -0700

    Initial Commit

---
+++ TabLayout.js
@@ -0,0 +1,39 @@
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import {
+  ColorPropType,
+  processColor,
+  requireNativeComponent,
+  View
+} from 'react-native';
+
+export default class TabLayout extends Component {
+  static propTypes = {
+    ...View.propTypes,
+    onTabSelected: PropTypes.func,
+    selectedTab: PropTypes.number,
+    selectedTabIndicatorColor: ColorPropType,
+    tabGravity: PropTypes.oneOf(['fill', 'center']),
+    tabMode: PropTypes.oneOf(['fixed', 'scrollable'])
+  };
+
+  onTabSelected: Function = (e) => {
+    if (this.props.onTabSelected) {
+      this.props.onTabSelected(e);
+    }
+  };
+
+  render() {
+    return (
+      <AndroidTabLayout
+        {...this.props}
+        onTabSelected={this.onTabSelected}
+        selectedTabIndicatorColor={processColor(this.props.selectedTabIndicatorColor)}
+        style={[{height: 48}, this.props.style]}/>
+    );
+  }
+}
+
+const AndroidTabLayout = requireNativeComponent('TabLayout', TabLayout);
